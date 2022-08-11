dda0bec6d4d9d4e22bb0447782b1825e87d86c90
Zachary Warnes <zacharywarnes@gmail.com>
2022-08-10 18:38:42 -0700

    added remainder of prompt questions and init function from inquirer documentation

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
-// TODO: Include packages needed for this application
+// Packages required for this application 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        message:"Please input the title of your program",
+        name:"title",
+    },
+    {
+        type: "input",
+        message: "Please enter a Description of your program",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please enter the Installation Instructions for your program",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Please enter the Usage Information for your program",
+        name: "usage",
+
+        type: "input",
+        message: "Please enter the Contribution Guidelines for your program",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "Please enter the Test Instructions for your program",
+        name: "test",
+    },
+    {
+        type: "list",
+        message: "Please choose a license for your program",
+        choice: [],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Please enter your GitHub username",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Please enter your email",
+        name: "email",
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+        ...questions
+    ])
+    .then((response) => {
+        writeToFile(response);
+    });
+
+}
 
 // Function call to initialize app
 init();
